cd0b0afbc10f320ef04ce4ee40fece9a969e9527
salguero <svjchrysler@gmail.com>
2016-07-29 10:40:05 -0400

    border radius

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container" style="background-color: #FFF;">
+<div class="container" style="background-color: #FFF; border-radius: 0 0 1em 1em;">
 
   <div class="row">
     <div class="col-md-12" style="padding:0;">
